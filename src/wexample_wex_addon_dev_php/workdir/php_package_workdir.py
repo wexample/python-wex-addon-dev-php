2c6e7165bfd973f02c3879fd6f32aa2fdafe8ff4
weeger <romain.weeger@gmail.com>
2025-08-29 09:12:06 +0200

    Base config

--- src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from wexample_config.const.types import DictConfig
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 
 class PhpPackageWorkdir(PhpWorkdir):
-    """Workdir placeholder for a PHP package."""
-    pass
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+
+        raw_value = super().prepare_value(raw_value=raw_value)
+
+        return raw_value
--- src/wexample_wex_addon_dev_php/workdir/php_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_workdir.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
-class PhpWorkdir:
+from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+
+
+class PhpWorkdir(CodeBaseWorkdir):
     """Base workdir for PHP projects (stub)."""
     pass
