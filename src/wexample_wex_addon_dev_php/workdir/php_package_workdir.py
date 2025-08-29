d6a2d27562d466e9e3cd38fbcb9cfbf159be072c
weeger <romain.weeger@gmail.com>
2025-08-29 08:52:29 +0200

    Rect

--- src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class PhpPackageWorkdir:
     """Workdir placeholder for a PHP package."""
     pass
--- src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 class PhpPackagesSuiteWorkdir:
     """Workdir placeholder for a suite of PHP packages."""
     pass
