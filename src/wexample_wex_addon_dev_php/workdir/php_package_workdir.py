76d8c4328db63311eaf8f1d38bcbd0ee912ce6c2
weeger <romain.weeger@gmail.com>
2025-08-29 09:41:50 +0200

    Base config

--- src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from wexample_config.const.types import DictConfig
-from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 from wexample_filestate.const.disk import DiskItemType
+from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
+
 
 class PhpPackageWorkdir(PhpWorkdir):
     def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
-
         raw_value = super().prepare_value(raw_value=raw_value)
 
         # Ensure a composer.json file exists for any PHP package project
@@ -20,3 +20,9 @@ class PhpPackageWorkdir(PhpWorkdir):
         )
 
         return raw_value
+
+    def get_dependencies(self) -> list[str]:
+        return []
+
+    def get_package_name(self) -> str:
+        return self.get_project_name()
