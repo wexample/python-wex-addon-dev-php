939c93d67432bd89299cca44a36f6f3f217c0e0a
weeger <romain.weeger@gmail.com>
2025-08-29 09:19:38 +0200

    Base config

--- src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
@@ -1,10 +1,19 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from wexample_wex_core.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
 
+if TYPE_CHECKING:
+    from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+
 
 class PhpPackagesSuiteWorkdir(FrameworkPackageSuiteWorkdir):
-    """Workdir placeholder for a suite of PHP packages."""
-    pass
+    def _get_children_default_workdir_class(self) -> type[CodeBaseWorkdir]:
+        from wexample_wex_addon_dev_php.workdir.php_package_workdir import (
+            PhpPackageWorkdir,
+        )
+
+        return PhpPackageWorkdir
