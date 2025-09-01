af3427a591868455c458bd8af6bbd17e86bf8653
weeger <romain.weeger@gmail.com>
2025-09-01 09:53:35 +0200

    Workdirs

--- src/wexample_wex_addon_dev_php/workdir/php_laravel_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_laravel_workdir.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 
 
-class PhpSymfonyWorkdir(PhpWorkdir):
+class PhpLaravelWorkdir(PhpWorkdir):
     pass
