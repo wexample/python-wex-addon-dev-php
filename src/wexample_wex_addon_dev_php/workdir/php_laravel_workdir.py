52725a0a67cf461258669a760953e14df8a569b6
weeger <romain.weeger@gmail.com>
2025-09-01 10:39:12 +0200

    Config php

--- src/wexample_wex_addon_dev_php/workdir/php_laravel_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_laravel_workdir.py
@@ -1,7 +1,29 @@
 from __future__ import annotations
 
+from wexample_config.const.types import DictConfig
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 
 
 class PhpLaravelWorkdir(PhpWorkdir):
-    pass
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+        from wexample_filestate.const.disk import DiskItemType
+
+        raw_value = super().prepare_value(raw_value)
+
+        children = raw_value["children"]
+
+
+        children.extend(
+            [
+                {
+                    "name": "tests",
+                    "type": DiskItemType.DIRECTORY,
+                    "should_exist": True,
+                    "children": [
+                        self._create_python_file_children_filter(),
+                    ],
+                },
+            ]
+        )
+
+        return raw_value
