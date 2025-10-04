b8073428baee988335538a9ee99cca913ad6a9c2
weeger <romain.weeger@gmail.com>
2025-10-04 20:32:05 +0200

    Move wex workdir

--- src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_packages_suite_workdir.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from wexample_wex_core.workdir.framework_packages_suite_workdir import (
+from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
 
 if TYPE_CHECKING:
     from pathlib import Path
 
-    from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+    from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 class PhpPackagesSuiteWorkdir(FrameworkPackageSuiteWorkdir):
