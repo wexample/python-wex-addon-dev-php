15f16269bc03080da0d2ca739a63bbf33b5ebee5
weeger <romain.weeger@gmail.com>
2025-11-28 15:10:07 +0100

    Install

--- src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
+from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import FrameworkPackageSuiteWorkdir
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 
 
 class PhpPackageWorkdir(PhpWorkdir):
-    pass
+    def _get_suite_package_workdir_class(self) -> type[FrameworkPackageSuiteWorkdir]:
+        from wexample_wex_addon_dev_php.workdir.php_packages_suite_workdir import (
+            PhpPackagesSuiteWorkdir,
+        )
+
+        return PhpPackagesSuiteWorkdir
