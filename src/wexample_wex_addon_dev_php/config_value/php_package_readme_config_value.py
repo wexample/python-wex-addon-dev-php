084bc6858709b04cd60b8eafe41cea222dca141f
weeger <romain.weeger@gmail.com>
2025-12-01 11:18:00 +0100

    Php readme

---
+++ src/wexample_wex_addon_dev_php/config_value/php_package_readme_config_value.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from wexample_helpers.decorator.base_class import base_class
+from wexample_wex_addon_app.config_value.app_readme_config_value import AppReadmeConfigValue
+
+
+@base_class
+class PhpPackageReadmeContentConfigValue(AppReadmeConfigValue):
+    """README generation for Php packages."""
+    pass
--- src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/php_package_workdir.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from wexample_wex_addon_app.workdir.framework_packages_suite_workdir import (
     FrameworkPackageSuiteWorkdir,
 )
-
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
+
+if TYPE_CHECKING:
+    from wexample_filestate.config_value.readme_content_config_value import (
+        ReadmeContentConfigValue,
+    )
 
 
 class PhpPackageWorkdir(PhpWorkdir):
@@ -14,3 +20,10 @@ class PhpPackageWorkdir(PhpWorkdir):
         )
 
         return PhpPackagesSuiteWorkdir
+
+    def _get_readme_content(self) -> ReadmeContentConfigValue | None:
+        from wexample_wex_addon_dev_php.config_value.php_package_readme_config_value import (
+            PhpPackageReadmeContentConfigValue,
+        )
+
+        return PhpPackageReadmeContentConfigValue(workdir=self)
