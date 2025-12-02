e29e53a68d7881c790ff77c6d7bf422d16d01438
weeger <romain.weeger@gmail.com>
2025-12-02 08:45:45 +0100

    Version

--- src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
+++ src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from wexample_filestate.item.file.json_file import JsonFile
 from wexample_helpers.decorator.base_class import base_class
+
+if TYPE_CHECKING:
+    from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 @base_class
@@ -10,3 +15,19 @@ class PhpComposerJsonFile(JsonFile):
         self, optional: bool = False, group: str = "dev"
     ) -> dict[str, str]:
         return self.read_config().search(path="require", default=[]).to_dict()
+
+    def dumps(self, content: dict | None = None) -> str:
+        import json
+
+        content = content or self.read_parsed()
+
+        package = self.find_package_workdir()
+        if package:
+            content["version"] = package.get_project_version()
+
+        return json.dumps(content or {}, ensure_ascii=False, indent=2)
+
+    def find_package_workdir(self) -> CodeBaseWorkdir | None:
+        from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
+
+        return self.find_closest(CodeBaseWorkdir)
