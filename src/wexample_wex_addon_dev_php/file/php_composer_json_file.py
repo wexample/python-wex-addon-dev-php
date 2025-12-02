be7401e8e4d1999a048e870d203bf7c393d9d63c
weeger <romain.weeger@gmail.com>
2025-12-02 14:48:40 +0100

    Propagate version

--- src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
+++ src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 from wexample_filestate.item.file.json_file import JsonFile
 from wexample_helpers.decorator.base_class import base_class
+from wexample_helpers.helpers.debug import debug_dump
 
 if TYPE_CHECKING:
     from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
@@ -11,6 +12,43 @@ if TYPE_CHECKING:
 
 @base_class
 class PhpComposerJsonFile(JsonFile):
+    def add_dependency(
+            self, package_name: str, version: str, operator: str = "", optional: bool = False, group: None|str = None
+    ) -> bool:
+        """
+        Add or update a Composer dependency.
+        Returns True if the dependency list changed.
+        """
+
+        group_key = "require-dev" if group == "dev" else "require"
+        version = f"{operator}{version}"
+
+        config = self.read_config()
+
+        # Ensure group exists
+        deps = config.search(path=group_key, default={}).to_dict_or_none()
+
+        old_version = deps.get(package_name)
+
+        # If version is unchanged → nothing to do
+        if old_version == version:
+            return False
+
+        # Apply change
+        deps[package_name] = version
+
+        config_updated = {}
+        config_updated[group_key] = deps
+
+        debug_dump(group_key)
+
+        config.update_nested(data=config_updated)
+        self._content_cache_config = config
+
+        self.write_config(config)
+
+        return True
+
     def get_dependencies_versions(
         self, optional: bool = False, group: str = "dev"
     ) -> dict[str, str]:
