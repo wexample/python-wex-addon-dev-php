46c446716b7e5f5d0839de70493386c712945217
weeger <romain.weeger@gmail.com>
2025-12-03 07:12:16 +0100

    Class parent

--- src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
+++ src/wexample_wex_addon_dev_php/file/php_composer_json_file.py
@@ -1,12 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from wexample_filestate.item.file.json_file import JsonFile
 from wexample_helpers.decorator.base_class import base_class
-
-if TYPE_CHECKING:
-    from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
 
 
 @base_class
@@ -68,14 +63,6 @@ class PhpComposerJsonFile(JsonFile):
         import json
 
         content = content or self.read_parsed()
-
-        package = self.find_package_workdir()
-        if package:
-            content["version"] = package.get_project_version()
+        content["version"] = self.get_parent_item().get_project_version()
 
         return json.dumps(content or {}, ensure_ascii=False, indent=2)
-
-    def find_package_workdir(self) -> CodeBaseWorkdir | None:
-        from wexample_wex_addon_app.workdir.code_base_workdir import CodeBaseWorkdir
-
-        return self.find_closest(CodeBaseWorkdir)
