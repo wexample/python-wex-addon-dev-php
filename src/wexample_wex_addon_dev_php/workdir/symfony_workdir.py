5a282895cabe83746ec94c504eede5cb1102666b
weeger <romain.weeger@gmail.com>
2025-12-04 20:26:23 +0100

    Symfony

--- src/wexample_wex_addon_dev_php/workdir/symfony_workdir.py
+++ src/wexample_wex_addon_dev_php/workdir/symfony_workdir.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from wexample_filestate.result.file_state_result import FileStateResult
 from wexample_wex_addon_dev_javascript.workdir.javascript_workdir import JavascriptWorkdir
 from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
 
@@ -15,3 +16,19 @@ class SymfonyWorkdir(PhpWorkdir):
         )
 
         workdir_javascript.libraries_sync()
+
+    def apply(
+            self,
+            **kwargs,
+    ) -> FileStateResult:
+        result = super().apply(**kwargs)
+
+        workdir_javascript = JavascriptWorkdir.create_from_path(
+            path=self.get_path()
+        )
+
+        workdir_javascript.apply(
+            **kwargs
+        )
+
+        return result
