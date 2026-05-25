afe6dcf5ca854e4ad54709559b83705b65922375
weeger <romain.weeger@gmail.com>
2026-05-25 15:12:29 +0200

    Use cli package

--- src/wexample_wex_addon_dev_php/services/wordpress/commands/url/replace.py
+++ src/wexample_wex_addon_dev_php/services/wordpress/commands/url/replace.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from wexample_wex_core.const.globals import COMMAND_TYPE_SERVICE
-from wexample_wex_core.decorator.command import command
-from wexample_wex_core.decorator.option import option
+from wexample_cli.decorator.command import command
+from wexample_cli.decorator.option import option
 
 if TYPE_CHECKING:
     from wexample_app.response.abstract_response import AbstractResponse
     from wexample_wex_addon_app.service.app_service import AppService
-    from wexample_wex_core.context.execution_context import ExecutionContext
+    from wexample_cli.context.execution_context import ExecutionContext
 
 
 @option(
