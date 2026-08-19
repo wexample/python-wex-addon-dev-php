0db465e2e15042e314fd7a1acb8a3de9652234f3
weeger <romain.weeger@gmail.com>
2026-08-19 14:36:05 +0200

    Types

--- src/wexample_wex_addon_dev_php/php_addon_manager.py
+++ src/wexample_wex_addon_dev_php/php_addon_manager.py
@@ -14,3 +14,22 @@ class PhpAddonManager(AbstractAddonManager):
         import wexample_wex_addon_dev_php
 
         return wexample_wex_addon_dev_php
+
+    def get_workdir_types(self) -> dict[str, type]:
+        from wexample_wex_addon_dev_php.workdir.php_package_workdir import (
+            PhpPackageWorkdir,
+        )
+        from wexample_wex_addon_dev_php.workdir.php_packages_suite_workdir import (
+            PhpPackagesSuiteWorkdir,
+        )
+        from wexample_wex_addon_dev_php.workdir.php_symfony_workdir import (
+            PhpSymfonyWorkdir,
+        )
+        from wexample_wex_addon_dev_php.workdir.php_workdir import PhpWorkdir
+
+        return {
+            "php": PhpWorkdir,
+            "php-package": PhpPackageWorkdir,
+            "php-packages-suite": PhpPackagesSuiteWorkdir,
+            "php-symfony": PhpSymfonyWorkdir,
+        }
